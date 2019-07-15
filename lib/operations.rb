5b0339939993204d6733e698594aa954c9f672f7
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-15 18:12:38 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -7,20 +7,12 @@ def unsafe?(speed)
     false 
   end
 end
-=begin 
-if statements return true than the code that follows will execute 
-age < 2 ? Baby "baby" : "not a baby"
-if age is less than true reutn baby 
-else retrun not a baby
-? is the if : is the else 
-so if true the right side of the colon will be return else it will be left side. 
-
-=end 
 
 
 def not_safe?(speed)
 	speed > 60 || speed < 40 ? true : false
 end
+
 	
 
 
