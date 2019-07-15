fd3f79d9f3ded3dba10ce22d03eb5206b5ca3762
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-15 18:05:00 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,7 +1,13 @@
 def unsafe?(speed)
 
 end
-
+=begin 
+if statements return true than the code that follows will execute 
+age < 2 ? Baby "baby" : "not a baby"
+if age is less than true reutn baby 
+else retrun not a baby
+? is the if : is the else 
+so if true the right side of the colon will be return else it will be left side. 
 
 
 def not_safe?(speed)
