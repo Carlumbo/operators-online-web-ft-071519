4ab7e8b675a1a8db555bf13aaa6024302f9cecb1
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-15 18:13:15 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -14,5 +14,16 @@ def not_safe?(speed)
 end
 
 	
+=begin 
+if statements return true than the code that follows will execute 
+age < 2 ? Baby "baby" : "not a baby"
+if age is less than true reutn baby 
+else retrun not a baby
+? is the if : is the else 
+so if true the right side of the colon will be return else it will be left side.
+? if do this, else do this 
+pipes are an or symbol, but you hvae to use the varibles again
+
+=end 
 
 
